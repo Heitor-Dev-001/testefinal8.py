eb97843771f8740e51073275868df245f5690865
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:29:32 -0300

    Add files via upload

---
+++ testefinal008.py
@@ -0,0 +1,4 @@
+#Qual das opções a seguir permite ler a 
+# idade de uma pessoa em Python, já considerando o valor digitado como um número?
+
+idade = int (input('Quantos anos você tem?')) # O input serve para entrada de dados do usuário
